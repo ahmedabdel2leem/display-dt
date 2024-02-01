fdb181e89dc53742c72bfa6347ccadb8e63dc9ef
Div-AHM1ED <ahmedabdelaleem100@gmail.com>
2024-02-01 16:36:11 +0200

    first

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Data from './data';
+
+
+
+function App() {
+
+  return (
+    <div>
+<Data/>
+    </div>
+
+    
+  );
+  
+}
+
+export default App;
